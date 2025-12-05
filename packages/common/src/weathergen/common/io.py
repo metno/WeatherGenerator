62bb4853797c493eb1ff6ab0eb93aa96d227184d
amelie <146197392+a3eille@users.noreply.github.com>
2025-12-05 11:37:15 +0100

    made a fix so output works also when source interval does not exist

--- packages/common/src/weathergen/common/io.py
+++ packages/common/src/weathergen/common/io.py
@@ -228,9 +228,12 @@ class OutputDataset:
             arrays: Data and Coordinate arrays.
             attrs: Additional metadata.
         """
-        assert "source_interval" in attrs, "missing expected attribute 'source_interval'"
+        # assert "source_interval" in attrs, "missing expected attribute 'source_interval'"
+        if "source_interval" in attrs:
+            source_interval = TimeRange(**attrs.pop("source_interval"))
+        else:
+            source_interval = TimeRange(1, 2)
 
-        source_interval = TimeRange(**attrs.pop("source_interval"))
         return cls(name, key, source_interval, **arrays, **attrs)
 
     @functools.cached_property
