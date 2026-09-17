8739e8b37bfd66ea89fb6c6f476a46a6d2064bfc
a3eille <146197392+a3eille@users.noreply.github.com>
2026-09-17 17:16:49 +0200

    fix for inspecting zarr files with missing step 0

--- packages/common/src/weathergen/common/io.py
+++ packages/common/src/weathergen/common/io.py
@@ -483,6 +483,8 @@ class ZarrIO:
 
     @functools.cached_property
     def forecast_offset(self) -> int:
+        if self.example_key.forecast_step != 0:
+            return 1
         fstep0_datasets = self._get_datasets(self.example_key)
         return ItemKey._infer_forecast_offset(fstep0_datasets)
 
@@ -491,7 +493,7 @@ class ZarrIO:
         try:
             sample, example_sample = next(self.data_root.groups())
             stream, example_stream = next(example_sample.groups())
-            fstep = 0
+            fstep = min(int(step) for step in example_stream.group_keys())
         except StopIteration as e:
             msg = f"Data store at: {self._store_path} is empty."
             raise FileNotFoundError(msg) from e
@@ -517,12 +519,11 @@ class ZarrIO:
         _, example_sample = next(self.data_root.groups())
         _, example_stream = next(example_sample.groups())
 
-        all_steps = sorted(list(example_stream.group_keys()))
+        all_steps = sorted(example_stream.group_keys(), key=int)
 
-        if self.forecast_offset == 1:
+        if self.forecast_offset == 1 and all_steps and int(all_steps[0]) == 0:
             return all_steps[1:]  # exclude fstep with no targets/preds
-        else:
-            return all_steps
+        return all_steps
 
 
 class ZipZarrIO(ZarrIO):
